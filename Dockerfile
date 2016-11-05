461236d35acae39b4f9f06c7c6671337e43e47d1
Nico Coetzee <nicc777@gmail.com>
2016-11-05 10:16:16 +0200

    Changed again...

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
 FROM ubuntu:latest
 MAINTAINER nicc777@gmail.com
 
+RUN apt-get update
+RUN apt-get upgrade -y
+RUN apt-get install -y postgresql postgresql-contrib
 RUN mkdir /opt/scripts
+COPY scripts/* /opt/scripts/
+RUN chmod 750 /opt/scripts/*.sh
 
 EXPOSE 5432
-
-VOLUME  ["/etc/postgresql", "/var/log/postgresql", "/var/lib/postgresql", "/opt"]
 
 CMD ["sh", "/opt/scripts/start_db.sh"]
 
--- scripts/start_db.sh
+++ scripts/start_db.sh
@@ -6,9 +6,6 @@ then
     echo "PostgreSQL is ready for use [1]"
 else
     echo "Preparing PostgreSQL for first use"
-    apt-get update
-    apt-get upgrade -y
-    apt-get install -y postgresql postgresql-contrib
     service postgresql stop
     echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/`ls -l /etc/postgresql | awk '{print $9}' | sort -nr | head -1`/main/pg_hba.conf
     echo "listen_addresses='*'" >> /etc/postgresql/`ls -l /etc/postgresql | awk '{print $9}' | sort -nr | head -1`/main/postgresql.conf
@@ -21,6 +18,8 @@ else
 fi
 
 
-
+while [ 1 ]; do
+    sleep 60
+done
 
 
