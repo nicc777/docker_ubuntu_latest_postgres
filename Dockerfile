5a34e1c087a98ae6b9ab15bcc53231a62b255bbe
Nico Coetzee <nicc777@gmail.com>
2016-11-05 09:56:18 +0200

    Ready for fin tuning

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,11 @@
 FROM ubuntu:latest
 MAINTAINER nicc777@gmail.com
 
-RUN apt-get update
-RUN apt-get install -y postgresql postgresql-contrib
-
-
 RUN mkdir /opt/scripts
-COPY opt/scripts/* /opt/scripts/
-RUN chmod 750 /opt/scripts/*.sh
-RUN chmod 644 /opt/scripts/*.sql
-RUN /opt/scripts/start_db.sh 
+
+EXPOSE 5432
 
 VOLUME  ["/etc/postgresql", "/var/log/postgresql", "/var/lib/postgresql", "/opt"]
 
-CMD ["/opt/scripts/start_db.sh"]
+CMD ["sh", "/opt/scripts/start_db.sh"]
 
--- persistence/opt/scripts/start_db.sh
+++ persistence/opt/scripts/start_db.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-if [ -e "/opt/postgresql_prepped" ]
+if [ -e "/opt/scripts/postgresql_prepped" ]
 then
     service postgresql start
     echo "PostgreSQL is ready for use [1]"
 else
     echo "Preparing PostgreSQL for first use"
+    apt-get update
+    apt-get upgrade -y
+    apt-get install -y postgresql postgresql-contrib
     service postgresql stop
     echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/`ls -l /etc/postgresql | awk '{print $9}' | sort -nr | head -1`/main/pg_hba.conf
     echo "listen_addresses='*'" >> /etc/postgresql/`ls -l /etc/postgresql | awk '{print $9}' | sort -nr | head -1`/main/postgresql.conf
@@ -13,7 +16,7 @@ else
     sleep 5
     su -l postgres -c "psql -f /opt/scripts/create_user.sql"
     su -l postgres -c "createdb -O docker docker"
-    touch /opt/postgresql_prepped
+    touch /opt/scripts/postgresql_prepped
     echo "PostgreSQL is ready for use [2]"
 fi
 
