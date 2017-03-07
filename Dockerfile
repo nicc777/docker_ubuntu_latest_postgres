ebbcb1f8629ce9673cc45c6a2a5924793c003140
Nico Coetzee <nicc777@gmail.com>
2017-03-07 06:52:18 +0200

    Updated to support external data directory

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,24 @@
 FROM ubuntu:latest
 MAINTAINER nicc777@gmail.com
 
+ENV DATA_DIR default
+ENV DATA_LOCALE en_US.UTF-8
+ENV DATA_ENCODING UTF8
+
 RUN apt-get update
 RUN apt-get upgrade -y
-RUN apt-get install -y postgresql postgresql-contrib
+RUN apt-get install -y postgresql postgresql-contrib sudo mlocate
+
+RUN updatedb
+RUN locale-gen en_US.UTF-8
+RUN update-locale
+
 RUN mkdir /opt/scripts
 COPY scripts/* /opt/scripts/
 RUN chmod 750 /opt/scripts/*.sh
 
-RUN mkdir /opt/backups
-VOLUME ['/opt/backups'] 
+RUN mkdir -p /opt/backups /opt/data
+VOLUME ['/opt/backups', '/opt/data'] 
 
 EXPOSE 5432
 
--- scripts/start_db.sh
+++ scripts/start_db.sh
@@ -1,28 +1,48 @@
 #!/bin/sh
 
-if [ -e "/opt/scripts/postgresql_prepped" ]
+if [ -d "$DATA_DIR" ] 
 then
-    service postgresql start
-    echo "PostgreSQL is ready for use [1]"
+	echo "Using Custom Data Directory (may be external to the container)"
+	if [ ! -e "$DATA_DIR/postgresql_prepped" ]
+	then
+		sudo -u postgres `locate initdb | grep bin | grep postgres | head -1` --encoding=$DATA_ENCODING  --locale=$DATA_LOCALE -D $DATA_DIR 
+		touch $DATA_DIR/postgresql_prepped
+    		echo "host all  all    0.0.0.0/0  md5" >> $DATA_DIR/pg_hba.conf
+    		echo "listen_addresses='*'" >> $DATA_DIR/postgresql.conf
+		sudo -u postgres `locate pg_ctl | grep bin | grep postgres | head -1` start -D $DATA_DIR
+    		sleep 5
+    		su -l postgres -c "psql -f /opt/scripts/create_user.sql"
+    		su -l postgres -c "createdb -O docker docker"
+    		echo "PostgreSQL is ready for use [3]"
+	else
+		sudo -u postgres `locate pg_ctl | grep bin | grep postgres | head -1` start -D $DATA_DIR
+    		echo "PostgreSQL is ready for use [4]"
+	fi
 else
-    echo "Preparing PostgreSQL for first use"
-    service postgresql stop
-    echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/`ls -l /etc/postgresql | awk '{print $9}' | sort -nr | head -1`/main/pg_hba.conf
-    echo "listen_addresses='*'" >> /etc/postgresql/`ls -l /etc/postgresql | awk '{print $9}' | sort -nr | head -1`/main/postgresql.conf
-    service postgresql start
-    sleep 5
-    su -l postgres -c "psql -f /opt/scripts/create_user.sql"
-    su -l postgres -c "createdb -O docker docker"
-    touch /opt/scripts/postgresql_prepped
-    echo "PostgreSQL is ready for use [2]"
+	if [ -e "/opt/scripts/postgresql_prepped" ]
+	then
+    		service postgresql start
+    		echo "PostgreSQL is ready for use [1]"
+	else
+    		echo "Preparing PostgreSQL for first use"
+    		service postgresql stop
+    		echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/`ls -l /etc/postgresql | awk '{print $9}' | sort -nr | head -1`/main/pg_hba.conf
+    		echo "listen_addresses='*'" >> /etc/postgresql/`ls -l /etc/postgresql | awk '{print $9}' | sort -nr | head -1`/main/postgresql.conf
+    		service postgresql start
+    		sleep 5
+    		su -l postgres -c "psql -f /opt/scripts/create_user.sql"
+    		su -l postgres -c "createdb -O docker docker"
+    		touch /opt/scripts/postgresql_prepped
+    		echo "PostgreSQL is ready for use [2]"
+	fi
 fi
 
 if [ -e "/opt/backups/backup.sql" ]
 then
-    su -l postgres -c "psql -d docker -f /opt/backups/backup.sql"
-    echo "Backup Restored"
+	su -l postgres -c "psql -d docker -f /opt/backups/backup.sql"
+	echo "Backup Restored"
 else
-    echo "No backup found - starting with a clean DB"
+	echo "No backup found - starting with a clean DB"
 fi
 
 while [ 1 ]; do
